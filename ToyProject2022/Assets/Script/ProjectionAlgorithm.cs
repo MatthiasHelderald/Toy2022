3e9abf797417e3ff4d75682ec9015e517bc973b6
MatthiasHelderald <109383974+MatthiasHelderald@users.noreply.github.com>
2022-10-20 15:20:35 +0200

    ?

--- ToyProject2022/Assets/Script/ProjectionAlgorithm.cs
+++ ToyProject2022/Assets/Script/ProjectionAlgorithm.cs
@@ -4,16 +4,40 @@ using UnityEngine;
 
 public class ProjectionAlgorithm : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject c_green;
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        RaycastHit hit;
+        if (Physics.Raycast(c_green.transform.position, transform.TransformDirection(Vector3.forward), out hit, 0.5f))
+        {
+
+        }
+
+        if (Physics.Raycast(c_green.transform.position, transform.TransformDirection(Vector3.back), out hit, 0.5f))
+        {
+
+        }
+
+        if (Physics.Raycast(c_green.transform.position, transform.TransformDirection(Vector3.right), out hit, 0.5f))
+        {
+
+        }
+
+        if (Physics.Raycast(c_green.transform.position, transform.TransformDirection(Vector3.left), out hit, 0.5f))
+        {
+
+        }
+
+        else
+        {
+            Debug.Log("nothing");
+        }
     }
-    
 }
--- ToyProject2022/Assets/Script/Snapper.cs
+++ ToyProject2022/Assets/Script/Snapper.cs
@@ -2,14 +2,31 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Snapper : MonoBehaviour
 {
     public List<GameObject> cubetypes;
     public List<GameObject> cubeinput;
     public List<Vector3> cube_pos;
+    public GameObject c_green;
     public int count = 0;
-    
+    private int v1;
+
+    private void Start()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(c_green.transform.position, c_green.transform.TransformDirection(Vector3.forward), out hit, 0.5f))
+        {
+            Debug.Log("cube!");
+        }
+                
+        else
+        {
+            Debug.Log("nothing");
+        }
+    }
+
     void Update()
     {
         if (Input.GetKeyDown("1"))
@@ -38,21 +55,59 @@ public class Snapper : MonoBehaviour
                 Vector3 position = hit.transform.position + hit.normal;
 
                 // calculate the rotation to create the object aligned with the face normal:
-                Quaternion rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+                //Quaternion rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
                 // create the object at the face center, and perpendicular to it:
                 GameObject Placement = Instantiate(cubetypes[count]);
                 cubeinput.Add((cubetypes[count]));
                 cube_pos.Add(position);
+                Random.Range(5f,10f);
                 Placement.transform.position = position;
-                Placement.transform.rotation = rotation;
+                //Placement.transform.rotation = rotation;
                 
-
-
                 //Instantiate( PrimitiveType.Cube as GameObject , position , rotation ) as GameObject;
             }
             else
             {
                 Debug.Log("nothing");
+            }
+            
+            if (Physics.Raycast(c_green.transform.position, c_green.transform.TransformDirection(Vector3.forward), out hit, 1f))
+            {
+                Debug.Log("cube devant!");
+            }
+                
+            else
+            {
+                Debug.Log("rien devant");
+            }
+
+            if (Physics.Raycast(c_green.transform.position, c_green.transform.TransformDirection(Vector3.back), out hit, 1f))
+            {
+                Debug.Log("cube derrière!");
+            }
+            else
+            {
+                Debug.Log("rien derrière");
+            }
+            
+            if (Physics.Raycast(c_green.transform.position, c_green.transform.TransformDirection(Vector3.right), out hit, 1f))
+            {
+                Debug.Log("cube à droite!");
+            }
+            
+            else
+            {
+                Debug.Log("rien à droitee");
+            }
+
+            if (Physics.Raycast(c_green.transform.position, c_green.transform.TransformDirection(Vector3.left), out hit, 1f))
+            {
+                Debug.Log("cube à gauche!");
+            }
+            
+            else
+            {
+                Debug.Log("rien à gauche");
             }
             
         }
